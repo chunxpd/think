e67514a34de2a161a3c47336ed312ad57cec0f9d
chunxpd <chunxpd@naver.com>
2018-04-13 15:26:04 +0900

    20180413 레이아웃 완료

--- app/src/main/java/chunxpd/company/app/think/MainActivity.java
+++ app/src/main/java/chunxpd/company/app/think/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 import android.os.Process;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -14,6 +16,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
     private final long FINISH_INTERVAL_TIME = 2000;
     private long   backPressedTime = 0;
+
+
+    ViewPager vp;
+    LinearLayout ll;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +51,118 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        vp = (ViewPager)findViewById(R.id.vp);
+        ll = (LinearLayout)findViewById(R.id.ll);
+
+        TextView tab_first = (TextView)findViewById(R.id.tab_first);
+        TextView tab_second = (TextView)findViewById(R.id.tab_second);
+        TextView tab_third = (TextView)findViewById(R.id.tab_third);
+
+        vp.setAdapter(new pagerAdapter(getSupportFragmentManager()));
+        vp.setCurrentItem(0);
+
+        tab_first.setOnClickListener(movePageListener);
+        tab_first.setTag(0);
+        tab_second.setOnClickListener(movePageListener);
+        tab_second.setTag(1);
+        tab_third.setOnClickListener(movePageListener);
+        tab_third.setTag(2);
+
+        tab_first.setSelected(true);
+
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener()
+        {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+            {
+
+            }
+
+            @Override
+            public void onPageSelected(int position)
+            {
+                int i = 0;
+                while(i<3)
+                {
+                    if(position==i)
+                    {
+                        ll.findViewWithTag(i).setSelected(true);
+                    }
+                    else
+                    {
+                        ll.findViewWithTag(i).setSelected(false);
+                    }
+                    i++;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state)
+            {
+
+            }
+        });
+
+
 
     }
+    View.OnClickListener movePageListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            int tag = (int) v.getTag();
 
+            int i = 0;
+            while(i<3)
+            {
+                if(tag==i)
+                {
+                    ll.findViewWithTag(i).setSelected(true);
+                }
+                else
+                {
+                    ll.findViewWithTag(i).setSelected(false);
+                }
+                i++;
+            }
+
+            vp.setCurrentItem(tag);
+        }
+    };
+    private class pagerAdapter extends FragmentStatePagerAdapter
+    {
+        public pagerAdapter(android.support.v4.app.FragmentManager fm)
+        {
+            super(fm);
+        }
+        @Override
+        public android.support.v4.app.Fragment getItem(int position)
+        {
+            switch(position)
+            {
+                case 0:
+                    return new Fragment1();
+                case 1:
+                    return new Fragment2();
+                case 2:
+                    return new Fragment3();
+                default:
+                    return null;
+            }
+        }
+        @Override
+        public int getCount()
+        {
+            return 3;
+        }
+    }
+    private class WebViewClientClass extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String overrideUrl) {
+            view.loadUrl(overrideUrl);
+            return true;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
